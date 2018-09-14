e27b38da19e7552b58c8b646376b2b4b61979dac
VitahaKryvenko <gerzoh1@gmail.com>
2018-09-14 21:40:24 +0300

    home page

---
+++ build/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const mainMenu = document.getElementById('main-menu');
+document.getElementById('main-menu-button').onclick = function () {
+    this.classList.toggle('slide-button-open');
+    mainMenu.classList.toggle('slide-menu-show');
+};
