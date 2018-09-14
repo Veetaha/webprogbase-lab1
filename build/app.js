e147aa228f012b56d68b24d830fe066c69e981bb
VitahaKryvenko <gerzoh1@gmail.com>
2018-09-15 00:51:56 +0300

    added users.html

--- build/app.js
+++ build/app.js
@@ -1,4 +1,21 @@
 "use strict";
+document.getElementById('menu-placeholder').innerHTML = `
+<div id="main-menu-button" class="slide-button">
+        <div class="slide-button-bar slide-button-top"></div>
+        <div class="slide-button-bar slide-button-middle"></div>
+        <div class="slide-button-bar slide-button-bottom"></div>
+    </div>
+    <div class="slide-menu" id="main-menu">
+        <div class='slide-menu-content'>
+            <ul class="menu" >
+                <li><a href="index.html">Home</a></li>
+                <li><a href="users.html">Users</a></li>
+                <li><a href="tasks.html">Tasks</a></li>
+                <li><a href="about.html">About</a></li>
+            </ul>
+        </div>
+    </div>
+`;
 const mainMenu = document.getElementById('main-menu');
 document.getElementById('main-menu-button').onclick = function () {
     this.classList.toggle('slide-button-open');
